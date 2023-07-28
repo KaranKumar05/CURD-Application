608b61d1eaae73df2e3cf273a3c2050aa5500503
KARAN KUMAR <kkvmalhi@gmail.com>
2023-07-29 00:13:32 +0500

    minnor changes remain

---
+++ public/js/script.js
@@ -0,0 +1,23 @@
+let selectedRow = null;
+
+function showAlert(message, className){
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+
+    const container = document.querySelector('container');
+    const main = document.querySelector('main');
+    container.insertBefore(div, main);
+
+    setTimeout(()=> document.querySelector('.alert').remove(),3000);
+}
+
+// Delete Button 
+document.querySelector('.deleteBtn').addEventListener('click', (e)=>{
+//    alert('Delete')
+target = e.target;
+if(target.classList.contains('delete')){
+    target.parentElement.parentElement.remove();
+    showAlert('Data Deleted Successfully','danger')
+}
+});
